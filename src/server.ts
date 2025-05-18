39df27484f3c2c769a5aef0c82b5236f7307791f
James Harris <3440094+wopian@users.noreply.github.com>
2025-05-18 17:21:00 +0100

    fix: disable rate limiter until CloudFlare client IP addressed

--- src/server.ts
+++ src/server.ts
@@ -92,20 +92,24 @@ export async function buildServer(db = realDb) {
 		maxAge: 86400,
 	});
 
+	/*
 	await app.register(ratelimit, {
 		max: 100,
 		timeWindow: '1 minute',
 		exponentialBackoff: true,
 	});
+	*/
 
 	// Aggresively rate limit 404 routes
 	app.setNotFoundHandler(
 		{
+			/*
 			preHandler: app.rateLimit({
 				max: 100, // TODO: decrease to 2 once GTR mod is live
 				timeWindow: '5 minutes',
 				exponentialBackoff: true,
 			}),
+			*/
 		},
 		(_, reply) => {
 			reply.status(404).send({
